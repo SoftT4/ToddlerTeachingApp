bdebba27b069606695a9c4d13ab1a7c2c1eba20d
poon1112 <parichat.tanai@mavs.uta.edu>
2021-04-17 02:19:05 -0500

    Team4

---
+++ app/src/main/java/com/example/team4/User.java
@@ -0,0 +1,85 @@
+package com.example.team4;
+
+public class User {
+    String name, username, email, phone, password, boy, girl;
+    int age;
+
+    public User() {
+
+    }
+
+    public User(String name, String username, String email, String phone, String password, String boy, String girl, int age) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+        this.boy = boy;
+        this.girl = girl;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getBoy() {
+        return boy;
+    }
+
+    public void setBoy(String boy) {
+        this.boy = boy;
+    }
+
+    public String getGirl() {
+        return girl;
+    }
+
+    public void setGirl(String girl) {
+        this.girl = girl;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
